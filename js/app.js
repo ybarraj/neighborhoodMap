536630190209a0e33656d09349f3628c84505442
ybarraj <josephybarra88@gmail.com>
2016-11-17 19:14:53 -0700

    Map showing up and first marker on the map

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat:40.7413549, lng: -73.9980244},
-		zoom: 13
+		center: {lat:40.843434, lng: -111.933587},
+		zoom: 12
+	});
+	var geocoder = new google.maps.Geocoder();
+	document.getElementById('submit').addEventListener('click', function(){
+		geocodeAddress(geocoder, map);
+	})
+	var slc = {lat: 40.760779, lng: -111.891047};
+	var marker = new google.maps.Marker({
+		position: slc, 
+		map: map,
+		title: 'First Marker'
 	});
 }
+
+
+
+
